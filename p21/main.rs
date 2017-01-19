a1b1b9544f8e6ceff4c0b89cd597980a92039fd0
yuya yano <yuya.yano.6260@gmail.com>
2017-01-19 16:06:22 +0900

    ディレクトリ構成の変更

--- p21/main.rs
+++ p21/main.rs
@@ -1,8 +1,8 @@
 fn q(n: usize, a: &[i32]) -> i32 {
     let mut max = 0;
     for i in 0..n {
-        for j in i+1..n {
-            for k in j+1..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
                 if a[i] + a[j] < a[k] || a[j] + a[k] < a[i] || a[k] + a[i] < a[j] {
                     continue;
                 }
@@ -18,22 +18,25 @@ fn q(n: usize, a: &[i32]) -> i32 {
 
 fn q_alt(mut v: Vec<i32>) -> i32 {
     v.sort_by(|a, b| b.cmp(a));
-    v[2..].iter().fold((v[0], v[1], 0), |(v1, v2, sum), x| {
-        if v1 > v2 + x {
-            return (v2, *x, 0);
-        }
-        if sum < v1 + v2 + x {
-            return (v2, *x, v1 + v2 + x);
-        }
-        (v2, *x, sum)
-    }).2
+    v[2..]
+        .iter()
+        .fold((v[0], v[1], 0), |(v1, v2, sum), x| {
+            if v1 > v2 + x {
+                return (v2, *x, 0);
+            }
+            if sum < v1 + v2 + x {
+                return (v2, *x, v1 + v2 + x);
+            }
+            (v2, *x, sum)
+        })
+        .2
 }
 
-fn main(){
+fn main() {
     let n: usize = 5;
     let a = [2, 3, 4, 5, 10];
     println!("{}", q(n, &a));
 
-    let v = vec![2, 3, 4, 5, 10];
+    let v: Vec<i32> = vec![2, 3, 4, 5, 10];
     println!("{}", q_alt(v));
 }
---
+++ p23/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let v: Vec<i32> = vec![1, 2, 3];
+    println!("{:?}", v);
+}
